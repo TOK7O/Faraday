cab0ccb16a5fde6cc0c9f6c0169f71a3ffd1a998
Rumel <100099007+Rumeleq@users.noreply.github.com>
2026-02-09 19:54:57 +0100

    dashboard protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import HomePage from "./pages/home/HomePage";
 import { useTranslation } from "./context/LanguageContext";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const { t } = useTranslation();
@@ -16,7 +17,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
       <Route path="/docs" element={<Documentation />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
