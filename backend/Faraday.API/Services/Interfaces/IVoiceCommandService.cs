d414127314c12658e01389e34c0e83f86ec8f2a0
TOK7O <tok7ov@gmail.com>
2026-02-09 17:21:26 +0100

    Add VoiceCommandService

---
+++ backend/Faraday.API/Services/Interfaces/IVoiceCommandService.cs
@@ -0,0 +1,9 @@
+using Faraday.API.DTOs;
+
+namespace Faraday.API.Services.Interfaces
+{
+    public interface IVoiceCommandService
+    {
+        Task<VoiceCommandResponseDto> ProcessVoiceCommandAsync(string commandText, int userId);
+    }
+}
